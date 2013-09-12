fa07633868e6a2d85f6e473c8c2e925f9f34ff61
colitman <colitman@rambler.ru>
2013-09-12 20:05:36 +0300

    reorganization :)

--- server/01 - compile.bat
+++
@@ -1,2 +0,0 @@
-javac -Xlint:unchecked -d cls -classpath bin\lib\log4j-1.2.16.jar src\server\com\netcracker\romenskiy\gui\*.java src\server\com\netcracker\romenskiy\gui\model\*.java src\server\com\netcracker\romenskiy\messages\*.java src\server\com\netcracker\romenskiy\*.java src\client\com\netcracker\romenskiy\*.java
-pause
--- server/02 - build.bat
+++
@@ -1,3 +0,0 @@
-jar cmf res\smanifest.mf bin\Server.jar -C cls\ server
-jar cmf res\cmanifest.mf bin\Client.jar -C cls\ client
-pause
